f8cef6b8456e09eb67e01264b1df854f74bc4239
AhmedMD72 <ahmedmedhat749d1@gmail.com>
2024-06-29 18:11:43 +0300

    TASK_5

---
+++ task_5/image_viewer/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/ROS_WS/src/image_viewer
+# Build directory: /home/ahmed/ROS_WS/src/image_viewer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
